853d136c92a1afabbc3345df1e0ab5d8850d690e
useryesa9@gmail.com <useryesa9@gmail.com>
2024-09-26 15:42:02 +0700

    menambahkan fungsi store untuk membuat data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
     public function index()
     {
         $users = User::all();
@@ -13,5 +14,21 @@ class UserController extends Controller {
             'success' => true,
             'data' => $users,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+        ]);
+
+        $user = User::create($validatedData);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Pengguna berhasil dibuat',
+            'data' => $user
+        ], 201);
     }
 }
