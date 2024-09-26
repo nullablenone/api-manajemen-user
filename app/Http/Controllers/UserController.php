141166f231f00d1441d6f9e4ead95f09f724e9b1
useryesa9@gmail.com <useryesa9@gmail.com>
2024-09-26 15:33:24 +0700

    menambahkan fungsi index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,4 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller {}
+class UserController extends Controller {
+    public function index()
+    {
+        $users = User::all();
+        return response()->json([
+            'success' => true,
+            'data' => $users,
+        ]);
+    }
+}
